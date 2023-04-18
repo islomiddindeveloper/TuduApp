c5e2714f38c4b2d7e91782526ea131ae5a4c28f9
islomiddindeveloper <islomiddincoder@gmail.com>
2023-04-18 23:20:42 +0500

    fghkjl

--- src/App.jsx
+++ src/App.jsx
@@ -28,114 +28,121 @@ function App() {
     }
     setDedline('')
     setTitle("")
-    setName ("")
+    setName("")
     setTel("")
   };
 
-  return (
-    <>
-      <div className="p-5 w-75 shadow m-5 rounded mx-auto ">
-        <h1 className="text-success fs-4 text-center">TASK MANAGER APP</h1>
-        <hr />
+  const removeTask = (id) => {
+    
+    let fillterTask = task.filter( (item) =>  item.id !== id)
+      
+  setTask(fillterTask)
+}
 
-        <form className="form" onSubmit={(e) => addTask(e)}>
-          <label htmlFor="task_title" className="w-50 d-block mx-auto m-5">
-            <p className="text-primary fw-bold text-uppercase">
-              NAME
-            </p>
-            <input
-              type="text"
-              id="task_title"
-              className="form-control p-3 w-100"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
+return (
+  <>
+    <div className="p-5 w-75 shadow m-5 rounded mx-auto ">
+      <h1 className="text-success fs-4 text-center">TASK MANAGER APP</h1>
+      <hr />
 
-          <label htmlFor="task_title" className="w-50 d-block mx-auto">
-            <p className="text-primary fw-bold text-uppercase">
-              FAMILYA
-            </p>
-            <input
-              type="text"
-              id="task_title"
-              className="form-control p-3 w-100"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </label>
-
-          <label htmlFor="task_title" className="w-50 d-block mx-auto m-5">
-            <p className="text-primary fw-bold text-uppercase">
-              TEL
-            </p>
-            <input
-              type="tel"
-              id="task_title"
-              className="form-control p-3 w-100"
-              value={tel}
-              onChange={(e) => setTel(e.target.value)}
-            />
-          </label>
-
-
-          <label htmlFor="task_title" className="w-50 d-block mx-auto my-5">
-            <p className="text-primary fw-bold text-uppercase">
-              DATE
-            </p>
-            <input
-              type="date"
-              id="task_title"
-              className="form-control p-3 w-100"
-              value={dedline}
-              onChange={(e) => setDedline(e.target.value)}
-            />
-          </label>
-
-          <button
-            type="submit"
+      <form className="form" onSubmit={(e) => addTask(e)}>
+        <label htmlFor="task_title" className="w-50 d-block mx-auto m-5">
+          <p className="text-primary fw-bold text-uppercase">
+            NAME
+          </p>
+          <input
+            type="text"
             id="task_title"
-            className="btn btn-success w-50 mx-auto d-block p-3"
-          >
-            ADD
-          </button>
-        </form>
-      </div>
-      <div className="shadow p-5 w-75 mx-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>NAME</th>
-              <th>TEL</th>
-              <th>ID</th>
-              <th>TITLE</th>
-              <th>DEADLINE</th>
-              <th>REMOVE</th>
+            className="form-control p-3 w-100"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
 
-            </tr>
-          </thead>
+        <label htmlFor="task_title" className="w-50 d-block mx-auto">
+          <p className="text-primary fw-bold text-uppercase">
+            FAMILYA
+          </p>
+          <input
+            type="text"
+            id="task_title"
+            className="form-control p-3 w-100"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
 
-          <tbody>
-            {task?.map((item, index) => {
-              return (
-                <tr>
-                  <td>{item.name}</td>
-                  <td>{item.tel}</td>
-                  <td>{index}</td>
-                  <td>{item.title}</td>
-                  <td>{item.dedline}</td>
-                  <td>
-                    {" "}
-                    <button className="btn btn-danger">DELETE</button>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+        <label htmlFor="task_title" className="w-50 d-block mx-auto m-5">
+          <p className="text-primary fw-bold text-uppercase">
+            TEL
+          </p>
+          <input
+            type="tel"
+            id="task_title"
+            className="form-control p-3 w-100"
+            value={tel}
+            onChange={(e) => setTel(e.target.value)}
+          />
+        </label>
+
+
+        <label htmlFor="task_title" className="w-50 d-block mx-auto my-5">
+          <p className="text-primary fw-bold text-uppercase">
+            DATE
+          </p>
+          <input
+            type="date"
+            id="task_title"
+            className="form-control p-3 w-100"
+            value={dedline}
+            onChange={(e) => setDedline(e.target.value)}
+          />
+        </label>
+
+        <button
+          type="submit"
+          id="task_title"
+          className="btn btn-success w-50 mx-auto d-block p-3"
+        >
+          ADD
+        </button>
+      </form>
+    </div>
+    <div className="shadow p-5 w-75 mx-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>NAME</th>
+            <th>TEL</th>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>DEADLINE</th>
+            <th>REMOVE</th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+          {task?.map((item, index) => {
+            return (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.tel}</td>
+                <td>{index}</td>
+                <td>{item.title}</td>
+                <td>{item.dedline}</td>
+                <td>
+                  {" "}
+                  <button onClick={ () => removeTask(item.id)} className="btn btn-danger">DELETE</button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  </>
+);
 }
 
 export default App;
